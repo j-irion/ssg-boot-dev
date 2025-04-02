a5d6f2f5ee74e790d41998d5bb9e9927b721879e
Julius Irion <juliusirion@gmx.de>
2025-04-02 23:53:50 +0200

    add LeafNode class and unit tests for to_html method

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -15,3 +15,22 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html_multiple(self):
         node = HTMLNode("div", "Hello, World!", props={"class": "container", "id": "main"})
         self.assertEqual(node.props_to_html(), 'class="container" id="main"')
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", props={"href": "https://www.example.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.example.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
